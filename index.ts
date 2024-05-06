a7f4831da1bf948eff5ecb5a73c423fd7acefff7
King Elisha <king.elisha@seeq.com>
2024-05-06 11:07:43 +0100

    More type exports

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
 export { useStore } from './src/hooks/useStore';
 export { useStoreProperty } from './src/hooks/useStoreProperty';
 export { Store, CentralStore } from './src/store';
-export { StoreOptions } from './src/store.types';
+export { StoreOptions, StoreActions, StoreCallback } from './src/store.types';
